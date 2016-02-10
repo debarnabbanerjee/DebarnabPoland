9ee7d4d64b825fe64291bc81d3be5ceeef046bbe
Debarnab Banerjee <debarnab.banerjee@gmail.com>
2016-02-10 19:12:20 +0100

    First commit

---
+++ src/com/debarnab/testcases/LoginTestCase.java
@@ -0,0 +1,14 @@
+package com.debarnab.testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginTestCase {
+	
+	@Test
+	public void loginTestCase(){
+		
+		System.out.println("Testing");	
+		
+	}
+
+}
